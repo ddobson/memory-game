2c31c1de367e4ff104a02c9960b61b853c51676f
Dale GA <dale-ga@Mac11535s-MacBook-Pro.local>
2016-12-11 14:08:53 -0500

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var cardOne = 'king';
+var cardTwo = 'king';
+var cardThree = 'queen';
+var cardFour = 'queen';
+
+if (cardOne === cardTwo || cardThree === cardFour) {
+  alert("You found a match!");
+} else {
+  alert("Sorry, try again.");
+}
