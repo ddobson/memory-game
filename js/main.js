a548d853dcd5f363326fb72caac0a016c4dfad8d
Dale Dobson <ddobson445@gmail.com>
2016-12-22 21:08:23 -0500

    Load cards with JS

--- js/main.js
+++ js/main.js
@@ -1,10 +1,23 @@
-var cardOne = 'king';
-var cardTwo = 'king';
-var cardThree = 'queen';
-var cardFour = 'queen';
+document.addEventListener('DOMContentLoaded', function(){
+      createCards();
+});
 
-if (cardOne === cardTwo || cardThree === cardFour) {
-  alert("You found a match!");
-} else {
-  alert("Sorry, try again.");
+function createCards() {
+  for (var i = 0; i < 4; i++) {
+    var gameBoard = document.getElementById('game-board');
+    var card = document.createElement("div");
+    card.className = "card";
+    gameBoard.appendChild(card);
+  }
 }
+
+// var cardOne = 'king';
+// var cardTwo = 'king';
+// var cardThree = 'queen';
+// var cardFour = 'queen';
+//
+// if (cardOne === cardTwo || cardThree === cardFour) {
+//   alert("You found a match!");
+// } else {
+//   alert("Sorry, try again.");
+// }
